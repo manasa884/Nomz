659bb6ee2b9a7fcf5980c8d57ead69dde80e953b
manasa884 <manasa.tipparam@gmail.com>
2015-12-29 19:37:49 -0800

    first commit

---
+++ Nomz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nomz
+//
+//  Created by Manasa Tipparam on 12/29/15.
+//  Copyright © 2015 Manasa Tipparam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
